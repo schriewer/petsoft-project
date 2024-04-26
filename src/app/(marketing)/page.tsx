54c687b7e963eec29a8e38a98620e7af08c3e40b
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-27 00:55:19 +0200

    Video 271

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -21,7 +23,14 @@ export default function Home() {
           Use PetSoft to easily keep track of pets under your care. Get lifetime
           access for $299.
         </p>
-        <div className="mt-10">test</div>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+          <Button asChild variant="secondary">
+            <Link href="/signup">Log in</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );

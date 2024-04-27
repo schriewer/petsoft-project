655a4e6050d89ae59c54d8e4c63d431f27e4efef
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-27 12:16:32 +0200

    Video 272

---
+++ src/app/(app)/app/layout.tsx
@@ -0,0 +1,16 @@
+import AppFooter from "@/components/app-footer";
+import AppHeader from "@/components/app-header";
+import BackgroundPattern from "@/components/background-pattern";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <BackgroundPattern />
+      <div className="max-w-[1050px] px-4 mx-auto">
+        <AppHeader />
+        {children}
+        <AppFooter />
+      </div>
+    </>
+  );
+}

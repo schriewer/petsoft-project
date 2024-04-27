b5ff6672bcbcfabe1225fb5d4241a1136cc855e7
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-27 22:51:59 +0200

    Video 275

---
+++ src/components/search-form.tsx
@@ -0,0 +1,7 @@
+export default function SearchForm() {
+    return (
+        <form className="h-full w-full">
+                <input className="w-full h-full bg-white/20" title="Search" placeholder="Enter search query" />
+        </form>
+    )
+}

c564ac00d222ccc577f0bbf5fdcaaaa99c3e9756
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-27 12:47:09 +0200

    Video 273

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <BackgroundPattern />
-      <div className="max-w-[1050px] px-4 mx-auto">
+      <div className="flex flex-col min-h-screen max-w-[1050px] px-4 mx-auto">
         <AppHeader />
         {children}
         <AppFooter />

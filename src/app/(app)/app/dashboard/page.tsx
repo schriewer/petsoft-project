3d1bb441647d5106f60251cc4418b96a0991366a
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-27 17:45:44 +0200

    Video 274

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Page() {
   return (
-    <div>Dashboard page</div>
-  )
+    <main>
+      <div className="flex items-center justify-between text-white py-6">
+<Branding />
+        <Stats />
+      </div>
+    </main>
+  );
 }

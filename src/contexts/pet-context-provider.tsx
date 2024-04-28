4a19b826074caa116b7eb9ccfc65440da70b0c11
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-28 22:18:29 +0200

    Video 281

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -1,28 +1,40 @@
 "use client";
 
 import { createContext, useState } from "react";
-import { Pet } from '../lib/type';
+import { Pet } from "../lib/type";
 
 type PetContextProviderProps = {
-    data: Pet[];
-    children: React.ReactNode;
-    }
+  data: Pet[];
+  children: React.ReactNode;
+};
 
 type TPetContext = {
-    pets: Pet[];
-    selectedPetId: string | null;
-}
-
-
+  pets: Pet[];
+  selectedPetId: string | null;
+  handleChangeSelectedPetId: (id: string) => void;
+};
 
 export const PetContext = createContext<TPetContext | null>(null);
 
-export default function PetContextProvider({ data, children }: PetContextProviderProps) {
+export default function PetContextProvider({
+  data,
+  children,
+}: PetContextProviderProps) {
   const [pets, setPets] = useState(data);
-  const [selectedPetId, setSelectedPetId] = useState(null);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  const handleChangeSelectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  }
 
   return (
-    <PetContext.Provider value={{pets, selectedPetId}}>
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+        handleChangeSelectedPetId,
+      }}
+    >
       {children}
     </PetContext.Provider>
   );

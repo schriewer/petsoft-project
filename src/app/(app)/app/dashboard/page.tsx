bcb4a88d4f48e9c7f29d09ef86bf26d2cbcbaa88
schriewer <52544452+schriewer@users.noreply.github.com>
2024-04-28 09:35:36 +0200

    Video 278

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -5,10 +5,19 @@ import PetList from "@/components/pet-list";
 import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
-export default function Page() {
+export default async function Page() {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+  if (!response.ok) {
+    throw new Error("Could not fetch pets");
+  }
+  const data = await response.json();
+  console.log(data);
+
   return (
     <main>
-      <div className="flex items-center justify-between text-white py-6">
+      <div className="flex items-center justify-between text-white py-8">
         <Branding />
         <Stats />
       </div>
@@ -18,7 +27,7 @@ export default function Page() {
         </div>
         <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
           <ContentBlock>
-            <PetList />
+            <PetList pets={data}/>
           </ContentBlock>
         </div>
 
@@ -27,7 +36,6 @@ export default function Page() {
             <PetDetails />
           </ContentBlock>
         </div>
-
       </div>
     </main>
   );
